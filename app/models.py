bdff1cfae8cc9e9a6ec6647d7bfa87cf15f6343c
Danielle Malcolm <danielle.malcolm@mymona.uwi.edu>
2019-03-13 20:37:35 +0000

    I think I fixed the flask migarate thingy

--- app/models.py
+++ app/models.py
@@ -12,29 +12,24 @@ class UserProfile(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(80))
     last_name = db.Column(db.String(80))
-    username = db.Column(db.String(80), unique=True)
-    password = db.Column(db.String(255), unique=True)
+    gender = db.Column(db.String(10))
+    email = db.Column(db.String(80))
+    location = db.Column(db.String(80))
+    bio = db.Column(db.Text(300))
+    created_on = db.Column(db.String(12))
+    photo = db.Column(db.String(80))
     
-    def __init__(self, first_name, last_name, username, password):
+    def __init__(self, first_name, last_name,gender,email,location,bio,created_on,photo):
         self.first_name = first_name
         self.last_name = last_name
-        self.username = username
-        self.password = generate_password_hash(password, method='pbkdf2:sha256')
-
-    def is_authenticated(self):
-        return True
-
-    def is_active(self):
-        return True
-
-    def is_anonymous(self):
-        return False
-
-    def get_id(self):
-        try:
-            return unicode(self.id)  # python 2 support
-        except NameError:
-            return str(self.id)  # python 3 support
+        self.gender = gender
+        self.email = email
+        self.location = location
+        self.bio = bio
+        self.created_on = created_on
+        self.photo = photo
+        
+        
 
     def __repr__(self):
-        return '<User %r>' % (self.username)
+        return "User: {0} {1}".format(self.firstname, self.lastname)
--- app/views.py
+++ app/views.py
@@ -5,6 +5,7 @@ Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
 This file creates your application.
 """
 import os
+import datetime
 from app import app, db, login_manager, allowed_uploads
 from flask import render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user, current_user, login_required
@@ -15,9 +16,17 @@ from .forms import ContactForm
 from werkzeug.utils import secure_filename
 
 
-###
-# Routing for your application.
-###
+
+@app.route('/')
+def home():
+    """Render website's home page."""
+    return render_template('home.html')
+
+
+@app.route('/about/')
+def about():
+    """Render the website's about page."""
+    return render_template('about.html')
 
 
 @app.route("/profile",  methods=["GET", "POST"])
@@ -31,10 +40,14 @@ def profile():
             Email = myform.email.data
             Location = myform.location.data
             Bibliography = myform.bibliography.data
+            Created = str(datetime.datetime.now()).split()[0]
            
             Profilepic = myform.profilepic.data
             filename = secure_filename(Profilepic.filename)
             Profilepic.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            
+            
+            user = UserProfile(F_name, L_name, Gender, Email, Location, Bibliography, Created, Profilepic)
             
             
             flash('Your profile has successfully been created', 'success')
@@ -42,8 +55,7 @@ def profile():
         else:
             
             flash("OOOPS Sorry something went worng, Try again")
-    return render_template('profileform.html', form=myform )
-
+    return render_template('profile.html', form=myform )
 
 def get_uploaded_images():
     uploads = []
@@ -53,70 +65,12 @@ def get_uploaded_images():
                 uploads.append(file)
 
     return uploads
-@app.route('/')
-def home():
-    """Render website's home page."""
-    return render_template('home.html')
-
-
-@app.route('/about/')
-def about():
-    """Render the website's about page."""
-    return render_template('about.html')
-
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    form = LoginForm()
-    if request.method == "POST":
-        # change this to actually validate the entire form submission
-        # and not just one field
-        if form.validate_on_submit():
-            # Get the username and password values from the form.
-            username = form.username.data
-            password = form.password.data
-
-            # using your model, query database for a user based on the username
-            # and password submitted
-            # store the result of that query to a `user` variable so it can be
-            # passed to the login_user() method.
-            user = UserProfile.query.filter_by(username=username).first()
-            
-            if user is not None and user.password == password:
-                # get user id, load into session
-                login_user(user)
-            
-            
-                # remember to flash a message to the user
-                flash("Login Successful", "success")
-                return redirect(url_for("secure") )  # they should be redirected to a secure-page route instead
-                
-            flash("login failed", 'danger')
-    return render_template("login.html", form=form)
-
-
-# user_loader callback. This callback is used to reload the user object from
-# the user ID stored in the session
-@login_manager.user_loader
-def load_user(id):
-    return UserProfile.query.get(int(id))
-
-###
-# The functions below should be applicable to all Flask apps.
-###
 
 @app.route('/secure-page')
 @login_required
 def secure():
     return render_template('secure.html')
 
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    
-    flash('Logged out', 'success')
-    return redirect(url_for("home"))
 
 @app.route('/<file_name>.txt')
 def send_text_file(file_name):
